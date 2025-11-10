43805fd6db818df241decdc779bd8276553a1abb
Adnan <adnan99999977>
2025-11-10 19:44:33 +0600

    add cars get mathod api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,8 +32,27 @@ async function run() {
   try {
     await client.connect();
 
-    //  cars collection
+    //  cars API
 
+    // get cars by email
+    app.get("/cars", async (req, res) => {
+      try {
+        const query = {};
+        const providerEmail = req.query.ProviderEmail;
+
+        if (providerEmail) {
+          query.providerEmail = providerEmail;
+        }
+
+        const cursor = carsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Failed to get cars" });
+      }
+    });
+    // get all cars
     app.get("/cars", async (req, res) => {
       try {
         const cursor = carsCollection.find();
@@ -45,6 +64,7 @@ async function run() {
       }
     });
 
+    // post a car in cars
     app.post("/cars", async (req, res) => {
       try {
         const data = req.body;
@@ -53,6 +73,120 @@ async function run() {
       } catch (err) {
         console.error(err);
         res.status(500).send({ message: "Failed to add car" });
+      }
+    });
+
+    // get a car by email
+    app.get("/cars/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const newObjectId = new ObjectId(id);
+        const result = await carsCollection.findOne({ _id: newObjectId });
+        res.status(201).send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Failed to get" });
+      }
+    });
+
+    // update car status by patch
+    app.patch("/cars/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { status } = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: { status: status },
+        };
+        const result = await carsCollection.updateOne(filter, updateDoc);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Failed to update status" });
+      }
+    });
+
+    // update car  by put
+    app.put("/cars/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const updateData = req.body;
+        const result = await carsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updateData }
+        );
+        res.status(200).send({ success: true, message: "Car updated", result });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ success: false, message: "Update failed" });
+      }
+    });
+
+    //   delete car from cars
+    app.delete("/cars/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const filter = { _id: new ObjectId(id) };
+
+        const result = await carsCollection.deleteOne(filter);
+
+        if (result.deletedCount === 1) {
+          res
+            .status(200)
+            .send({ success: true, message: "Car deleted successfully" });
+        } else {
+          res.status(404).send({ success: false, message: "Car not found" });
+        }
+      } catch (err) {
+        console.error(err);
+        res
+          .status(500)
+          .send({ success: false, message: "Failed to delete car" });
+      }
+    });
+
+    // booking API
+
+    // book a car
+    app.post("/booking", async (req, res) => {
+      try {
+        const data = req.body;
+        const result = await bookingCollection.insertOne(data);
+        res.send(result);
+        res.status(201).send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Failed to add car" });
+      }
+    });
+
+    // get booked car card
+    app.get("/booking", async (req, res) => {
+      const cursor = bookingCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // delete car from booking
+
+    app.delete("/booking/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const filter = { _id: id };
+
+        const result = await bookingCollection.deleteOne(filter);
+
+        if (result.deletedCount === 1) {
+          res
+            .status(200)
+            .send({ success: true, message: "Car deleted successfully" });
+        } else {
+          res.status(404).send({ success: false, message: "Car not found" });
+        }
+      } catch (err) {
+        console.error(err);
+        res
+          .status(500)
+          .send({ success: false, message: "Failed to delete car" });
       }
     });
 
